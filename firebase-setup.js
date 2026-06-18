e0a7bf630a1e7f08a34344718d4584b3b071df74
Pinstaria <yallhi297@gmail.com>
2026-06-18 15:22:35 +0800

    Update firebase-setup.js

--- firebase-setup.js
+++ firebase-setup.js
@@ -5,14 +5,14 @@ import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gsta
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCTGtUWvvDnU-P0jukNDlOabnQBc7-P6B4",
+    authDomain: "steal-5a3b3.firebaseapp.com",
+    projectId: "steal-5a3b3",
+    storageBucket: "steal-5a3b3.firebasestorage.app",
+    messagingSenderId: "455116615919",
+    appId: "1:455116615919:web:be33deaf56fbf737ef2b1a",
+    measurementId: "G-JQVZN1M9LX"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
