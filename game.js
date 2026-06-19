753ac9d6766a2f6262ce6d22ab533b5730e5cf7e
Pinstaria <yallhi297@gmail.com>
2026-06-19 08:33:56 +0800

    Update game.js

--- game.js
+++ game.js
@@ -218,7 +218,7 @@ function setupInputListeners() {
                 socket.emit('adminSpawnRequest', raritySelect.value);
             }
         });
-    }-+
+    }
 }
 
 // --- GAMEPLAY MECHANICS ---
